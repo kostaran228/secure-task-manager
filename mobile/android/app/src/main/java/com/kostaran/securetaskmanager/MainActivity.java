90dd04e896e7f6b2cc13a5c10f02f45cc6d0706b
kostaran228 <257145502+kostaran228@users.noreply.github.com>
2026-08-13 11:46:29 +0300

    Use native Android WebView for server connections

--- mobile/android/app/src/main/java/com/kostaran/securetaskmanager/MainActivity.java
+++ mobile/android/app/src/main/java/com/kostaran/securetaskmanager/MainActivity.java
@@ -1,5 +1,21 @@
 package com.kostaran.securetaskmanager;
 
+import android.os.Bundle;
+import android.webkit.JavascriptInterface;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getBridge().getWebView().addJavascriptInterface(new ServerNavigator(), "NativeTaskManager");
+    }
+
+    private class ServerNavigator {
+        @JavascriptInterface
+        public void openServer(String url) {
+            runOnUiThread(() -> getBridge().getWebView().loadUrl(url));
+        }
+    }
+}
